5f3a73449fc357b28292f72e2ca9fb7f9f2631b6
martin durcan <m.durcan8@nuigalway.ie>
2017-03-27 19:43:17 +0100

    tested some more

---
+++ IncNibbleValTest.asm
@@ -0,0 +1,21 @@
+Main:
+	CALL IncNibbleVal
+	CALL IncNibbleVal
+	CALL IncNibbleVal
+	CALL IncNibbleVal
+	CALL IncNibbleVal
+END
+
+; Increments Nibble Value
+IncNibbleVal:
+	;Increments nibble value when moving through nibble 1-4
+	XOR R4, R4, R4
+	SETBR R4, 2
+	XOR R6, R1, R4
+	JNZ R6, 4
+	;Returns nibble value back to the start
+	CLRBR R1, 2
+	SETBR R1, 0
+	RET
+	INC R1, R1
+RET	
